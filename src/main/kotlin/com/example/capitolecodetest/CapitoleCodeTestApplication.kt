47aa05a39c385a67675b5b6c158f438279b1fa73
RAF <ricardabril45@gmail.com>
2025-04-12 16:50:51 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/capitolecodetest/CapitoleCodeTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.capitolecodetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CapitoleCodeTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<CapitoleCodeTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/capitolecodetest/CapitoleCodeTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.capitolecodetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CapitoleCodeTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
